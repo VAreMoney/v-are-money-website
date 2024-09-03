35c8af80a0b7942b0e7e37422523546b7808cc0f
Lane Babuder <lane@tunedchaos.com>
2024-09-03 09:07:00 -0400

    Adding footer text

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,3 +1,16 @@
 <template name="FooterComponent">
-  <img src="@/assets/images/v-www-img-03.png" alt="join the movement" />
+  <div>
+    <img src="@/assets/images/v-www-img-03.png" alt="join the movement" />
+  </div>
+  <div class="footer-links">
+    V are Money is a <a href="https://degenstudios.media" target="_blank">Degen Studios</a> project.
+  </div>
 </template>
+<style scoped>
+.footer-links {
+  margin-top: 10px;
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+}
+</style>
